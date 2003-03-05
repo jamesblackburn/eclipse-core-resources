1d16b3d3e5fda32f6d6e21e17fd4dbeda5e20bf5
dwilson <dwilson>
2003-03-05 21:51:16 +0000

    32235 - [resources] OldModelObjectReader should not read project descriptions

--- src/org/eclipse/core/internal/resources/WorkspaceDescriptionReader.java
+++ src/org/eclipse/core/internal/resources/WorkspaceDescriptionReader.java
@@ -14,7 +14,6 @@ import java.io.*;
 import java.util.*;
 
 import javax.xml.parsers.*;
-import org.eclipse.core.internal.events.BuildCommand;
 import org.eclipse.core.internal.localstore.SafeFileInputStream;
 import org.eclipse.core.internal.utils.Policy;
 import org.eclipse.core.resources.*;
@@ -25,33 +24,12 @@ import org.xml.sax.SAXException;
  * This class contains legacy code only.  It is being used to read workspace
  * descriptions which are obsolete.
  */
-public class OldModelObjectReader implements IModelObjectConstants {
+public class WorkspaceDescriptionReader implements IModelObjectConstants {
 	/** constants */
 	protected static final IProject[] EMPTY_PROJECT_ARRAY = new IProject[0];
 	protected static final String[] EMPTY_STRING_ARRAY = new String[0];
 
-public OldModelObjectReader() {
-}
-protected Node getFirstChild(Node target, short type) {
-	NodeList list = target.getChildNodes();
-	for (int i = 0; i < list.getLength(); i++)
-		if (list.item(i).getNodeType() == type)
-			return list.item(i);
-	return null;
-}
-protected IProject[] getProjects(Node target) {
-	if (target == null)
-		return EMPTY_PROJECT_ARRAY;
-	NodeList list = target.getChildNodes();
-	if (list.getLength() == 0)
-		return EMPTY_PROJECT_ARRAY;
-	List result = new ArrayList(list.getLength());
-	for (int i = 0; i < list.getLength(); i++) {
-		Node node = list.item(i);
-		if (node.getNodeType() == Node.ELEMENT_NODE)
-			result.add(ResourcesPlugin.getWorkspace().getRoot().getProject((String) read(node.getChildNodes().item(0))));
-	}
-	return (IProject[]) result.toArray(new IProject[result.size()]);
+public WorkspaceDescriptionReader() {
 }
 protected String getString(Node target, String tagName) {
 	Node node = searchNode(target, tagName);
@@ -114,12 +92,6 @@ protected Object read(Node node) {
 		return null;
 	switch (node.getNodeType()) {
 		case Node.ELEMENT_NODE :
-			if (node.getNodeName().equals(BUILD_COMMAND))
-				return readBuildCommand(node);
-			if (node.getNodeName().equals(BUILD_SPEC))
-				return readBuildSpec(node);
-			if (node.getNodeName().equals(PROJECT_DESCRIPTION))
-				return readProjectDescription(node);
 			if (node.getNodeName().equals(WORKSPACE_DESCRIPTION))
 				return readWorkspaceDescription(node);
 		case Node.TEXT_NODE :
@@ -129,115 +101,9 @@ protected Object read(Node node) {
 			return node.toString();
 	}
 }
-protected BuildCommand readBuildCommand(Node node) {
-	// get values
-	String name = getString(node, NAME);
-	Hashtable arguments = readHashtable(searchNode(node, ARGUMENTS));
-
-	// build instance
-	BuildCommand command = new BuildCommand();
-	command.setName(name);
-	if (arguments != null)
-		command.setArguments(arguments);
-	return command;
-}
-protected ICommand[] readBuildSpec(Node node) {
-	if (node == null)
-		return null;
-	List results = new ArrayList(5);
-	NodeList list = node.getChildNodes();
-	for (int i = 0; i < list.getLength(); i++)
-		if (list.item(i).getNodeType() == Node.ELEMENT_NODE)
-			if (list.item(i).getNodeName().equals(BUILD_COMMAND))
-				results.add(readBuildCommand(list.item(i)));
-	return (ICommand[]) results.toArray(new ICommand[results.size()]);
-}
 /**
  * read (String, String) hashtables
  */
-protected Hashtable readHashtable(Node target) {
-	if (target == null)
-		return null;
-	Hashtable result = new Hashtable(5);
-	NodeList list = target.getChildNodes();
-	for (int i = 0; i < list.getLength(); i++) {
-		Node node = list.item(i);
-		if (node.getNodeType() == Node.ELEMENT_NODE)
-			if (node.getChildNodes().getLength() > 1) {
-				String name = getString(node, KEY);
-				String value = getString(node, VALUE);
-				if (name != null)
-					result.put(name, value == null ? "" : value); //$NON-NLS-1$
-			}
-	}
-	return result;
-}
-/**
- * Reads and returns the definition of a link description.  Returns null
- * if a link definition could not be read (missing or invalid information)
- */
-private LinkDescription readLinkDescription(Node node) {
-	String name = getString(node, NAME);
-	int type;
-	try {
-		type = Integer.parseInt(getString(node, TYPE));
-	} catch (NumberFormatException e) {
-		return null;
-	}
-	String location = getString(node, LOCATION);
-	if (name == null || location == null)
-		return null;
-	return new LinkDescription(name, type, new Path(location));
-}
-/**
- * Reads and returns the table of links, if any
- * @param node, may be null
- * @return HashMap, may be null
- */
-protected HashMap readLinks(Node node) {
-	if (node == null)
-		return null;
-	NodeList list = node.getChildNodes();
-	int numChildren = list.getLength();
-	if (numChildren <= 0)
-		return null;
-	HashMap result = new HashMap(numChildren*2+1);
-	for (int i = 0; i < numChildren; i++) {
-		Node item = list.item(i);
-		if (item.getNodeType() == Node.ELEMENT_NODE && item.getNodeName().equals(LINK)) {
-			LinkDescription link = readLinkDescription(item);
-			if (link != null)
-				result.put(link.getName(), link);
-		}
-	}
-	return result;
-}
-protected ProjectDescription readProjectDescription(Node node) {
-	// get values
-	String name = getString(node, NAME);
-	String comment = getString(node, COMMENT);
-	IProject[] projects = getProjects(searchNode(node, PROJECTS));
-	String location = getString(node, LOCATION);
-	ICommand[] buildSpec = readBuildSpec(searchNode(node, BUILD_SPEC));
-	HashMap links = readLinks(searchNode(node, LINKED_RESOURCES));
-	String[] natures = getStrings(searchNode(node, NATURES));
-	// build instance
-	ProjectDescription description = new ProjectDescription();
-	description.setName(name);
-	description.setComment(comment);
-	if (projects != null)
-		description.setReferencedProjects(projects);
-	if (location != null)
-		description.setLocation(new Path(location));
-	if (buildSpec != null)
-		description.setBuildSpec(buildSpec);
-	if (natures == null)
-		natures = EMPTY_STRING_ARRAY;
-	description.setNatureIds(natures);
-	if (links != null)
-		description.setLinkDescriptions(links);
-	return description;
-}
 protected WorkspaceDescription readWorkspaceDescription(Node node) {
 	// get values
 	String name = getString(node, NAME);
