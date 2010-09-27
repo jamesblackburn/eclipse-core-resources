d90a39e5aa867ec74d4789485b158ae7c4878e85
James Blackburn <jamesb@xl-cbga-20.(none)>
2010-09-27 15:15:48 +0100

    Don't expose internal methods as API. JavaDoc

--- src/org/eclipse/core/internal/resources/Project.java
+++ src/org/eclipse/core/internal/resources/Project.java
@@ -1380,7 +1380,7 @@ public class Project extends Container implements IProject {
 		ArrayList variants = new ArrayList(refs.length);
 		for (int i = 0; i < refs.length; i++) {
 			try {
-				variants.add(refs[i].getVariant());
+				variants.add(((ProjectVariantReference)refs[i]).getVariant());
 			} catch (CoreException e) {
 				// Ignore non-existant variant reference
 			}
@@ -1406,7 +1406,7 @@ public class Project extends Container implements IProject {
 				IProjectVariantReference[] refs = description.getAllVariantReferences(variants[j].getVariantName(), false);
 				for (int k = 0; k < refs.length; k++) {
 					try {
-						if (refs[k].getVariant().equals(variant)) {
+						if (((ProjectVariantReference)refs[k]).getVariant().equals(variant)) {
 							result.add(variants[j]);
 							break;
 						}
@@ -1445,7 +1445,7 @@ public class Project extends Container implements IProject {
 		IProjectVariant[] variants = getVariants();
 		for (int i = 0; i < variants.length; i++) {
 			if (variants[i].getVariantName().equals(name)) {
-				return (IProjectVariant) variants[i].clone();
+				return (ProjectVariant)((ProjectVariant) variants[i]).clone();
 			}
 		}
 		throw new ResourceException(IResourceStatus.PROJECT_VARIANT_NOT_FOUND, getFullPath(), null, null);
--- src/org/eclipse/core/resources/IBuildContext.java
+++ src/org/eclipse/core/resources/IBuildContext.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * Broadcom Corporation - initial API and implementation
  *******************************************************************************/
@@ -16,18 +16,22 @@ package org.eclipse.core.resources;
  * build (i.e. was explicitly asked to be built by the UI), or the result of
  * building a reference, and if so the portion of the reference graph that led
  * to it being built.
+ *
+ * @noimplement This interface is not intended to be implemented by clients.
+ * @noextend This interface is not intended to be extended by clients.
+ *
  * @since 3.7
  */
 public interface IBuildContext {
 	/**
 	 * Gets a list of projects that were built before this project variant, and
 	 * which this project variant required to be built.
-	 * 
+	 *
 	 * This includes indirect dependencies as well as direct dependencies. For example
 	 * if we have three projects P1, P2 and P3, where P1 references P2 and P2 references P3,
 	 * the list of all referenced projects for P1 (as part of building P1 and all its
-	 * references) will include _both_ P2 and P3, not just P2. 
-	 * 
+	 * references) will include _both_ P2 and P3, not just P2.
+	 *
 	 * @return the list of all referenced projects that have been built
 	 * in the current build; never null.
 	 */
@@ -36,10 +40,10 @@ public interface IBuildContext {
 	/**
 	 * Gets a list of project variants that were built before this project variant, and
 	 * which this project variant required to be built.
-	 * 
+	 *
 	 * This includes indirect dependencies as well as direct dependencies.
 	 * @see #getAllReferencedProjects
-	 * 
+	 *
 	 * @return the list of all referenced project variants that have been built
 	 * in the current build; never null.
 	 */
@@ -48,14 +52,14 @@ public interface IBuildContext {
 	/**
 	 * Gets a list of projects that will be built after this project variant,
 	 * and which reference this project variant.
-	 * 
+	 *
 	 * This includes indirect dependencies as well as direct dependencies.
 	 * @see #getAllReferencedProjects
-	 * 
+	 *
 	 * If the list is empty, then this build occurred due to a top-level request
 	 * (such as a UI action); not as a result of building another project variant's
 	 * references.
-	 * 
+	 *
 	 * @return a list of all referencing projects that will be built
 	 * in the current build; never null.
 	 */
@@ -64,14 +68,14 @@ public interface IBuildContext {
 	/**
 	 * Gets a list of project variants that will be built after this project variant,
 	 * and which reference this project variant.
-	 * 
+	 *
 	 * This includes indirect dependencies as well as direct dependencies.
 	 * @see #getAllReferencedProjects
-	 * 
+	 *
 	 * If the list is empty, then this build occurred due to a top-level request
 	 * (such as a UI action); not as a result of building another project variant's
 	 * references.
-	 * 
+	 *
 	 * @return a list of all referencing project variants that will be built
 	 * in the current build; never null.
 	 */
